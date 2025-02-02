8c35672a7725ed213559503dedc2e2182f123ddf
CodeAndCurrent786 <engr.tahiraleem@gmail.com>
2025-02-02 21:09:09 +0100

    Added the second python file

---
+++ second.py
@@ -0,0 +1,8 @@
+from first import Employee
+class Loan:
+    def __init__(self):
+        emp_1 = Employee('Alex','hales',30000)
+        self.name = self.emp_1.first
+        self.pay = self.emp_1.pay
+    def get_loan(self):
+        print(f"your pay is {self.pay} and you will get the loan")
